90b5c690a07e52725ce567f6e0bd02dba1dc173d
Frank de Jonge <info@frenky.net>
2012-10-06 11:53:45 +0200

    Multiton rewrite.

---
+++ src/Poser/Pose.php
@@ -0,0 +1,132 @@
+<?php
+
+namespace Poser;
+
+class Pose
+{
+	/**
+	 * @var  array  @map  alias mapping
+	 */
+	protected $map = array();
+
+	/**
+	 * @var  string  $current  class currently autoloaded
+	 */
+	protected $checkingClass;
+
+	/**
+	 * @var  bool  $registered  wether the autoloader function has been registered
+	 */
+	protected $registered = false;
+
+	/**
+	 * Autoloader function. Checks for a matching alias,
+	 * loads the destination class when found and creates
+	 * the class alias.
+	 *
+	 * @param   string  $classname  the class to load
+	 * @return  bool    wether the class was loaded
+	 */
+	public function load($classname)
+	{
+		if($classname === $this->checkingClass)
+		{
+			return false;
+		}
+
+		foreach ($this->map as $pattern => $replacement)
+		{
+			$pattern = str_replace('\\*', '(.*)', preg_quote($pattern, '#'));
+
+			if (preg_match('#^'.$pattern.'$#uD', $classname))
+			{
+				$replacement = str_replace('\\', '\\\\', $replacement);
+				$class = preg_replace('#^'.$pattern.'$#uD', $replacement, $classname);
+
+				if (class_exists($class, true))
+				{
+					class_alias($class, $classname);
+					
+					$this->checkingClass = null;
+
+					return true;
+				}
+			}
+		}
+
+		$this->checkingClass = null;
+
+		return false;
+	}
+
+	/**
+	 * Adds one or more aliasses to the map.
+	 *
+	 * @param   array   $aliasses  aliasses to add
+	 * @param   bool    $prepend   wether to prepend the aliasses to the map, false to append
+	 * @return  object  $this
+	 */
+	public function alias(array $aliasses, $prepend = false)
+	{
+		$this->map = $prepend ? array_merge($aliasses, $this->map) : array_merge($this->map, $aliasses);
+	
+		return $this;
+	}
+
+	/**
+	 * Removes one or more aliasses from the map.
+	 * This does not remove used aliasses.
+	 *
+	 * @param   mixed   $aliasses  aliasses to remove
+	 * @return  object  $this
+	 */
+	public function unalias(array $aliasses)
+	{
+		foreach ($aliasses as $pattern => $replacement)
+		{
+			if (is_numeric($pattern) and isset($this->map[$replacement]))
+			{
+				unset($this->map[$replacement]);
+			}
+			elseif (isset($this->map[$pattern]))
+			{
+				unset($this->map[$pattern]);
+			}
+		}
+		
+		return $this;
+	}
+
+	/**
+	 * Registers the autoloader function.
+	 *
+	 * @param   bool    $prepend  wether to prepend the loader to the autoloader stack.
+	 * @return  object  $this
+	 */
+	public function register($prepend = true)
+	{
+		if ( ! $this->registered)
+		{
+			spl_autoload_register(array($this, 'load'), true, $prepend);
+			$this->registered = true;
+		}
+		
+		return $this;
+	}
+
+	/**
+	 * Unregisters the autoloader function.
+	 *
+	 * @return  object  $this
+	 */
+	public function unregister()
+	{
+		if ($this->registered)
+		{
+			spl_autoload_unregister(array($this, 'load'));
+			$this->registered = false;
+		}
+		
+		return $this;
+	}
+}
--- src/Poser/Poser.php
+++ src/Poser/Poser.php
@@ -4,101 +4,34 @@ namespace Poser;
 
 class Poser
 {
-	/**
-	 * @var  array  @map  alias mapping
-	 */
-	protected static $map = array();
+	protected static $posers = array();
 
-	/**
-	 * @var  string  $current  class currently autoloaded
-	 */
-	protected static $checkingClass;
-	
-	protected static $registered = false;
-
-	/**
-	 * Autoloader function. Checks for a matching alias,
-	 * loads the destination class when found and creates
-	 * the class alias.
-	 *
-	 * @param   string  $classname  the class to load
-	 * @return  bool    wether the class was loaded
-	 */
-	public static function load($classname)
+	public static function instance($name)
 	{
-		if($classname === static::$checkingClass)
+		if ( ! isset(static::$posers[$name]))
 		{
-			return false;
+			static::$posers[$name] = new Pose();
 		}
-
-		foreach (static::$map as $pattern => $replacement)
-		{
-			$pattern = str_replace('\\*', '(.*)', preg_quote($pattern, '#'));
-
-			if (preg_match('#^'.$pattern.'$#uD', $classname))
-			{
-				$replacement = str_replace('\\', '\\\\', $replacement);
-				$class = preg_replace('#^'.$pattern.'$#uD', $replacement, $classname);
-
-				if (class_exists($class, true))
-				{
-					class_alias($class, $classname);
-					
-					static::$checkingClass = null;
-
-					return true;
-				}
-			}
-		}
-
-		static::$checkingClass = null;
-
-		return false;
+		
+		return static::$posers[$name];
 	}
 
 	/**
-	 * Adds one or more aliasses to the map.
+	 * Static call forwarder.
 	 *
-	 * @param  array  $aliasses  aliasses to add
-	 * @param  bool   @prepend   wether to prepend the aliasses to the map, false to append
+	 * @param   string  $func  called function
+	 * @param   array   $args  function arguments
+	 * @return  mixed   function result
 	 */
-	public static function alias(array $aliasses, $prepend = false)
+	public static function __callStatic($func, $args)
 	{
-		static::$map = $prepend ? array_merge($aliasses, static::$map) : array_merge(static::$map, $aliasses);
-	}
+		$instance = static::instance('__biggest_poser__');
 
-	/**
-	 * Removes one or more aliasses from the map.
-	 * This does not remove used aliasses.
-	 *
-	 * @param  mixed  $aliasses  aliasses to remove
-	 */
-	public static function unalias(array $aliasses)
-	{
-		foreach ($aliasses as $pattern => $replacement)
+		if ( ! method_exists($instance, $func))
 		{
-			if (is_numeric($pattern) and isset(static::$map[$replacement]))
-			{
-				unset(static::$map[$replacement]);
-			}
-			elseif (isset(static::$map[$pattern]))
-			{
-				unset(static::$map[$pattern]);
-			}
+			throw new \BadMethodCallException('Call to undefined method '.$func);
 		}
-	}
 
-	/**
-	 * Registers the autoloader function.
-	 *
-	 * @param  bool  $prepend  wether to prepend the loader to the autoloader stack.
-	 */
-	public static function register($prepend = true)
-	{
-		if ( ! static::$registered)
-		{
-			spl_autoload_register('Poser\Poser::load', true, $prepend);
-			static::$registered = true;
-		}
+		return call_user_func_array(array($instance, $func), $args);
 	}
 }
