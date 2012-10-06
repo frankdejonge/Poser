a982057c666eb149bb189b83b20ed2944ca5bebd
Frank de Jonge <info@frenky.net>
2012-10-06 12:23:35 +0200

    Tests

--- tests/PoserTests.php
+++ tests/PoserTests.php
@@ -4,6 +4,12 @@ use Poser\Poser;
 
 class PoserTests extends PHPUnit_Framework_TestCase
 {
+	public function setUp()
+	{
+		Poser::unregister();
+		Poser::register();
+	}
+
 	public function testClassAlias()
 	{	
 		Poser::alias(array(
@@ -12,5 +18,26 @@ class PoserTests extends PHPUnit_Framework_TestCase
 		
 		$this->assertTrue(Poser::load('AliasName'));
 		$this->assertFalse(Poser::load('OtherAliasName'));
+	}
+	
+	public function testClosureAlias()
+	{
+		Poser::alias(array(
+			'Get\*\This' => function($class)
+			{
+				return $class;
+			}
+		));
+		
+		$this->assertTrue(Poser::load('Get\\Poser\Pose\\This'));
+	}
+	
+	public function testAliassing()
+	{
+		Poser::alias(array(
+			'MyTestAlias' => 'Poser\Pose',
+		));
+		
+		$this->assertTrue(class_exists('MyTestAlias', true));
 	}
 }
